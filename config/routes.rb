030351879c2c488de8835a59d7e5117e662596d5
野田健成 <kensei1016ab@gmail.com>
2020-01-16 13:57:10 +0900

    ユーザは自分のタスクのみ扱えるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  namespace :admin ωdo
+  namespace :admin do
     resources :users
   end
   resources :tasks
