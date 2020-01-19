1a428e5db0c46397fb1f0b3ce1ed706cfc24f87f
野田健成 <kensei1016ab@gmail.com>
2020-01-19 14:44:02 +0900

    新規タスクに画像添付機能を追加（ActiveStrage)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
   before_validation :set_nameless_name
   validates :name, presence: true
   validates :name, length: {maximum: 30}
   validate :validate_name_not_including_comma
-
-  belongs_to :user
 
   scope :recent, -> {order(created_at: :desc)}
 
