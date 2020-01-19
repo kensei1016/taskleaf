ea08c94591ead0819777b1b9a343798210df31f5
野田健成 <kensei1016ab@gmail.com>
2020-01-19 16:09:12 +0900

    CSVのインポート機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,11 @@ class TasksController < ApplicationController
     render :new unless @task.valid?
   end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました。"
+  end
+
   def create
     # ↓は→と同じ @task = Task.new(task_params.merge(user_id: current_user.id))
     @task = current_user.tasks.new(task_params)
--- app/models/task.rb
+++ app/models/task.rb
@@ -32,12 +32,24 @@ class Task < ApplicationRecord
     ["name", 'description', "created_at", "updated_at"]
   end
 
+  # 現在の全てのtaskデータをCSVデータとして返却する
   def self.generate_csv
     CSV.generate(headers: true) do |csv|
       csv << csv_attributes
       all.each do |task|
         csv << csv_attributes.map{ |attr| task.send(attr) }
       end
+    end
+  end
+
+  # CSVファイルからtaskオブジェクトを生成
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      task = new
+      # to_hashにより {"name"=>xxx, "description"=>xxx ...} といったデータに変換される
+      # slice(*csv_attributes) は slice("name", "description" ...)と同じ
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
     end
   end
 end
